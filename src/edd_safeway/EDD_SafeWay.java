86d77ec5f758aa05a4a2b476bb42ff54a11349ca
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2020-10-07 14:05:48 -0600

    gitignore

--- src/edd_safeway/EDD_SafeWay.java
+++ src/edd_safeway/EDD_SafeWay.java
@@ -16,6 +16,8 @@ public class EDD_SafeWay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola Mundo!!");
     }
     
 }
